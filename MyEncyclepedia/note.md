885052f179d9899540bfc6be24075d1a6c6ebd83
Brayton Kerekffy <braytonkerekffy@Braytons-MacBook-Air.local>
2021-10-07 23:23:10 -0700

    added about pages

---
+++ MyEncyclepedia/note.md
@@ -0,0 +1,1 @@
+#How to kill a mocking bird
--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function openPage(evt, pageName) {
   document.getElementById(pageName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent("<h1>" + text + "</h1>"));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+// Start file download.
+document.getElementById("todaySubmit").addEventListener("click", function(){
+  // Generate download of hello.txt file with some content
+  var text = document.getElementById("todayNote").value;
+  var filename = "myFormattedNote.html";
+  
+  download(filename, text);
+}, false);
